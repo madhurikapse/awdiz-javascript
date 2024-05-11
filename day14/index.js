796e0ede2aea129e1588fec5cb08739ef93001d2
madhurikapse <120490305+madhurikapse@users.noreply.github.com>
2024-05-11 20:15:06 +0530

    day14js

--- day14/index.js
+++ day14/index.js
@@ -1,9 +1,31 @@
 //linear search
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var target = 4; // Compare  number.length / 2 -> 3
 
-var array=[1,2,3,4,5,6]
-var target=4
-for(var i=0;i<array.length;i++){
-    if(array[i]===target){
-        console.log(array[i])
+function find(array, target) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === target) {
+      console.log(i);
     }
+  }
+}
+find(numbers, target);
+
+
+var num=4
+while(num<22){
+   console.log(num)
+   num+=3
+}
+
+var num1=10
+while(num1>0){
+    console.log(num1)
+    num1--
+}
+
+var decending=20
+while(decending>0){
+    console.log(decending)
+    decending--
 }
